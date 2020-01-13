d91cd25f9b76a4ac28f4389777656ba3f2a05c40
SAJAL JAIN <sjain30sept@gmail.com>
2020-01-13 23:25:04 +0530

    Added Firestore

--- app/src/main/java/com/sajal/tasks/MainActivity.java
+++ app/src/main/java/com/sajal/tasks/MainActivity.java
@@ -9,14 +9,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
         Button button2 = findViewById(R.id.button2);
         Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
 
         firebaseDatabase= FirebaseDatabase.getInstance();
 
@@ -45,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
                 String strname = name.getText().toString();
                 String stremail = email.getText().toString();
                 String strpassword = password.getText().toString();
-
                 User.addUser(stremail,strname,strpassword);
+                User.addtoFirestore(stremail,strname,strpassword,getApplicationContext());
             }
         });
 
@@ -57,18 +64,21 @@ public class MainActivity extends AppCompatActivity {
                 reference.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        StringBuffer buffer = new StringBuffer();
-                            Map<String, Object> map = (Map<String, Object>)dataSnapshot.getValue();
-
-                            buffer.append("Email: " + map.get("Email") + "\n");
-                            buffer.append("Name: " + map.get("Name") + "\n");
-                            buffer.append("Password: " + map.get("Password") + "\n\n");
-                        Toast.makeText(MainActivity.this, buffer.toString(), Toast.LENGTH_SHORT).show();
+                           if (dataSnapshot.exists()) {
+                               StringBuffer buffer = new StringBuffer();
+                               Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                               buffer.append("Email: " + map.get("Email") + "\n");
+                               buffer.append("Name: " + map.get("Name") + "\n");
+                               buffer.append("Password: " + map.get("Password") + "\n\n");
+                               Toast.makeText(MainActivity.this, buffer.toString(), Toast.LENGTH_SHORT).show();
+                           }
+                           else
+                               Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError databaseError) {
-
+                        Toast.makeText(MainActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
             }
@@ -81,7 +91,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseFirestore firestore= FirebaseFirestore.getInstance();
+                firestore.collection("User").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (!queryDocumentSnapshots.isEmpty()){
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            StringBuffer buffer = new StringBuffer();
+                            for (DocumentSnapshot d: list) {
+                                User data = d.toObject(User.class);
+                                buffer.append("Name"+data.Name+"\n");
+                                buffer.append("Email"+data.Email+"\n");
+                                buffer.append("Password"+data.Password+"\n\n");
+//                                Toast.makeText(MainActivity.this, buffer.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            builder.setCancelable(true);
+                            builder.setTitle("Data");
+                            builder.setMessage(buffer.toString());
+                            builder.show();
+                        }
+                        else
+                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
+            }
+        });
     }
 }
 
--- app/src/main/java/com/sajal/tasks/User.java
+++ app/src/main/java/com/sajal/tasks/User.java
@@ -1,13 +1,34 @@
 package com.sajal.tasks;
 
+import android.content.Context;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class User {
     public String Email;
     public String Name;
     public String Password;
     private static FirebaseDatabase firebaseDatabase;
+    private static FirebaseFirestore firestore;
+
+    public User() {
+        this.Email = null;
+        this.Name =null;
+        this.Password =null;
+    }
 
     public User(String email, String name, String password) {
 
@@ -15,13 +36,42 @@ public class User {
         this.Name =name;
         this.Password =password;
     }
+
     public static boolean addUser(String email, String name, String password)
     {
         User user = new User(email,name,password);
-
         firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference myRef = firebaseDatabase.getReference("User");
         myRef.child(name).setValue(user);
+        return true;
+    }
+
+    public static boolean addtoFirestore(String email, String name, String password, final Context context){
+
+        User user = new User(email,name,password);
+        firestore = FirebaseFirestore.getInstance();
+        firestore.collection("User").document(name).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(context, "Data Added", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        /*firestore.collection("User").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(context, "Document ID"+documentReference.getId(), Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });  */
         return true;
     }
 
